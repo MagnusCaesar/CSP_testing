589865fc9c04897e04c19fe3f80f2a0e67b9e7b9
Magnus Caesar <vr.brucewayne.batman@gmail.com>
2021-10-15 14:08:49 +0530

    sqrt funct

---
+++ C/sqrt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float root=0;
+float avgF();
+void main()
+{
+   float a; 
+   scanf("%f", &a);
+   avgF(a);
+   printf("%f\n", root);
+}
+
+//average finde 
+float avgF(float h1)
+{
+   float h2 = 1.5*h1;
+   float h2s=h2*h2; 
+}
+
+
