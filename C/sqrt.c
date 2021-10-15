efda71923ab4dfeebf2cb1692ce1f7e3e6655532
Magnus Caesar <vr.brucewayne.batman@gmail.com>
2021-10-15 16:10:10 +0530

    fixed

--- C/sqrt.c
+++ C/sqrt.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 //main
 void main()
@@ -9,7 +10,7 @@ void main()
    float h2 = a;
    float h1 = 0;
    float av=a;
-   while (abs((av*av)-a)>0.0001) {
+   while (fabs((av*av)-a) > 0.0000000001) {
       if ((av*av)<a) {
          h1=av; 
       }
