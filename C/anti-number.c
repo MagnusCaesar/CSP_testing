a248d244ceaa7faf212fb42168f2b4ebb01c317f
Magnus Caesar <vr.brucewayne.batman@gmail.com>
2021-10-16 15:43:12 +0530

    anti-number

--- C/anti-number.c
+++ C/anti-number.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int q10();
+int r10();
+int q;
+int r;
+int b=0;
+
+void main()
+{
+   int a;
+   scanf("%d", &a);
+   q=a;
+   while (q!=0)
+   {
+      r=q%10;
+      q=q10(q);
+      b=(10*b)+r;
+   }
+   printf("%d\n", b);
+}
+
+int q10(int x)
+{
+   int s=x/10;
+   return s;
+}
